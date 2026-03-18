21539ce30b379500fb2093d50aa459203b525fa9
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2026-03-18 10:03:37 +0000

    Getting and parsing user input successfully

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello, World!');
+const validOptions = ['rock', 'paper', 'scissors'];
+
+function startGame() {
+     console.log('Welcome to Rock Paper Scissors!');
+    const userOption = process.argv.slice(2);
+    if (validOptions.includes(userOption[0].toLowerCase())) {
+        console.log(`You chose: ${userOption[0].toLowerCase()}`);
+    } else {
+        console.log('Invalid choice. Please choose rock, paper, or scissors.');
+    }
+}
+
+startGame();
+
+
+
+
+
+
+          
