255970acfaa6ca7321c8ad8895de978a00ef7d16
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2026-03-18 10:36:59 +0000

    Got as far as working out the first two characters

--- pl.js
+++ pl.js
@@ -1,7 +1,19 @@
+function translate(phrase) {
+    // Convert String to Array
+    const phraseArray = phrase.split(' ');
+    // Iterate over the Array and translate each word
+        phraseArray.forEach(word => {
+            // For each word, determine what the first 2 characters are and translate accordingly
+            const firstTwoChars = word.slice(0, 2);
+        }
+    
+}
+
 function startGame() {
      console.log('Welcome to the Pig Latin Translator!');
     const userPhrase = process.argv.slice(2)[0].toLowerCase();
     console.log(`You entered: ${userPhrase}`);
+    translate(userPhrase);
 }
 
 startGame();     
