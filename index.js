6bc4f0aec04af06554399450a9263cc68839165e
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2026-03-18 10:07:07 +0000

    Computer choice is added in

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const validOptions = ['rock', 'paper', 'scissors'];
 
+function getComputerChoice() {
+  const randomIndex = Math.floor(Math.random() * validOptions.length);
+  console.log(`Computer chose: ${validOptions[randomIndex]}`);
+  return validOptions[randomIndex];
+}
+
 function startGame() {
      console.log('Welcome to Rock Paper Scissors!');
     const userOption = process.argv.slice(2);
     if (validOptions.includes(userOption[0].toLowerCase())) {
         console.log(`You chose: ${userOption[0].toLowerCase()}`);
+        getComputerChoice();
     } else {
         console.log('Invalid choice. Please choose rock, paper, or scissors.');
     }
