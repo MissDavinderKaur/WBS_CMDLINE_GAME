c24139972504878414413be905170d0de270ad70
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2026-03-18 10:48:14 +0000

    Done but its ugly

--- pl.js
+++ pl.js
@@ -5,16 +5,23 @@ function translate(phrase) {
     // Convert String to Array
     const phraseArray = phrase.split(' ');
     // Iterate over the Array and translate each word
-        phraseArray.forEach(word => {
+        const translatedWords = phraseArray.map(word => {
             // For each word, determine what the first 2 characters are and translate accordingly
             const firstTwoChars = word.slice(0, 2);
             // If a word starts with a vowel add the word "way" at the end of the word.
-
-            // If a word starts with two consonants move the two consonants to the end of the word and add "ay."
-
-            // If a word starts with a consonant and a vowel, put the first letter of the word at the end of the word and add "ay."
-        }
-    
+            if (vowels.includes(firstTwoChars[0]) && vowels.includes(firstTwoChars[1])) {
+                return word + 'way';
+            } else if (consonants.includes(firstTwoChars[0]) && consonants.includes(firstTwoChars[1])) { 
+                // If a word starts with two consonants move the two consonants to the end of the word and add "ay."
+                return word.slice(2) + firstTwoChars + 'ay';
+            } else if (consonants.includes(firstTwoChars[0]) && vowels.includes(firstTwoChars[1])) {
+                // If a word starts with a consonant and a vowel, put the first letter of the word at the end of the word and add "ay."
+                return word.slice(1) + firstTwoChars[0] + 'ay';
+            }   
+        });
+        // Join the Array back into a String and return it.
+        const translatedPhrase = translatedWords.join(' ');
+        console.log(`Translated phrase: ${translatedPhrase}`);
 }
 
 function startGame() {
