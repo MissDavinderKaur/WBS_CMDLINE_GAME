74be0a50795b5d2f368301f89a73a2a3b79f7251
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2026-03-18 11:40:24 +0000

    Have sorted the new alphabet for encrypting

--- cc.js
+++ cc.js
@@ -1,5 +1,17 @@
-function encrypt(phrase) {
-    console.log(`Encrypting phrase: ${phrase}`);
+const alphabetArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+
+function encrypt(phrase, shift) {   
+    // Get new alphabet array based on the shift value
+    const shiftedAlphabetArray = alphabetArray.toSpliced(0, shift).concat(alphabetArray.slice(0,shift));
+    // Convert phase to an array
+    const phraseArray = phrase.split('');
+    // Iterate over the array and encrypt each letter 
+    const encryptedLetter = phraseArray.map(letter => {
+        console.log(`Current letter: ${letter}`);
+    });
+    // Join the array back into a String and return it.
+    const encryptedPhrase = encryptedLetter.join(' ');
+    console.log(`Encrypted phrase: ${encryptedPhrase}`);
 }
 
 function startGame() {
@@ -7,8 +19,7 @@ function startGame() {
     const userPhrase = process.argv.slice(2)[0].toLowerCase();
     const shift = parseInt(process.argv.slice(3)[0]);
     console.log(`You entered: ${userPhrase}`);
-    console.log(`Shift value: ${shift}`);
-    encrypt(userPhrase);
+    encrypt(userPhrase, shift);
 }
 
 startGame();     
