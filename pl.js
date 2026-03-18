50428f19dd1a960bc8fb601aaec6db6c757ee3a8
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2026-03-18 10:40:30 +0000

    these are the rules

--- pl.js
+++ pl.js
@@ -1,3 +1,6 @@
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+
 function translate(phrase) {
     // Convert String to Array
     const phraseArray = phrase.split(' ');
@@ -5,6 +8,11 @@ function translate(phrase) {
         phraseArray.forEach(word => {
             // For each word, determine what the first 2 characters are and translate accordingly
             const firstTwoChars = word.slice(0, 2);
+            // If a word starts with a vowel add the word "way" at the end of the word.
+
+            // If a word starts with two consonants move the two consonants to the end of the word and add "ay."
+
+            // If a word starts with a consonant and a vowel, put the first letter of the word at the end of the word and add "ay."
         }
     
 }
